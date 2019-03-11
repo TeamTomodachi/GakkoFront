1ae5051f42128ee05f677a0fa79cdb75310708c2
Windjy E. Jean <Yuugami@users.noreply.github.com>
2019-03-10 23:41:53 -0400

    Vertical Space Takeover
    The first row takes up the space that the second and third row are not taking. Works no matter the resolution. Sweet.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,23 +6,3 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {}
-
-/*
-* (03/03/2019)
-* To clean up and fix. It works, but not 100% of the time. Also doesn't 
-* take into account rotation (which might be fine). - Windjy
-*/
-window.onload = () => {
-  let traiSect = document.getElementById("trainerSection");
-  let achiSect = document.getElementById("achievementSection");
-  let traiNav = document.getElementById("trainerNav");
-
-  let bothParts = achiSect.offsetHeight + traiNav.offsetHeight;
-  // The height of the usable space.
-  let ionGridClientHeight = document.getElementsByTagName("ion-grid")[0].clientHeight;
-  // Space remaining
-  let leftOverSpace = 1 - (bothParts / ionGridClientHeight);
-  // Set the trainer section to the leftOverSpace.
-  let pct = ((1 - (bothParts / ionGridClientHeight))*100).toFixed(2) + "%";
-  traiSect.style.height = pct;
-}
