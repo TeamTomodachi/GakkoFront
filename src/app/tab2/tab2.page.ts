4ce812da982736f716f9336170304c082b61c56a
Windjy E. Jean <Yuugami@users.noreply.github.com>
2019-03-28 16:51:35 -0400

    GraphQL Dynamic Functions added
    Gave the GraphQL function dynamic capabilities. Client-side complete. Now awaiting the back-end integration to fetch the pokemon names off a user. Will refactor as necessary.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,24 +10,49 @@ import {
 
 export class Tab2Page {}
 
-var pokemonsJsonUrl = "https://graphql-pokemon.now.sh/?query=";
+async function queryAgainstPKMNDB(theThreePokes) {
 
-async function doquery() {
-    
+    var pokemonsJsonUrl = "https://graphql-pokemon.now.sh/?query=";
     const query = `
-    {
-        pokemon(name: "Pikachu") {
-          name
-          number
-          weaknesses
-        }
-    }      
-  `;
+        {
+            pkmn1: pokemon(name: "${theThreePokes[0]}") {
+                name
+                number
+            }
+            pkmn2: pokemon(name: "${theThreePokes[1]}") {
+                name
+                number
+            }
+            pkmn3: pokemon(name: "${theThreePokes[2]}") {
+                name
+                number
+            }
+        }      
+    `;
 
     const res = await fetch(pokemonsJsonUrl + query);
     const json = await res.json();
 
-    console.log(json);
+    // console.log(json);
+    return json;
 }
 
-doquery();
+function fetchPKMNImage({data}) {
+    const imageLinkP1 = "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/pokemon_icons/pokemon_icon_";
+    const imageLinkP2 = "_00.png";
+    const pkm1 = document.getElementById("pkmn1Img");
+    const pkm2 = document.getElementById("pkmn2Img");
+    const pkm3 = document.getElementById("pkmn3Img");
+
+    pkm1.setAttribute("src", imageLinkP1 + data.pkmn1.number + imageLinkP2);
+    pkm2.setAttribute("src", imageLinkP1 + data.pkmn2.number + imageLinkP2);
+    pkm3.setAttribute("src", imageLinkP1 + data.pkmn3.number + imageLinkP2);
+
+    console.log(data.pkmn1.number);
+}
+
+const arrayOfPokes = ["Abra", "Zapdos", "Squirtle"]
+
+addEventListener("load", async function(){
+    queryAgainstPKMNDB(arrayOfPokes).then(fetchPKMNImage);
+});
