15789819f828f77ad45a9816fb5f54215f41fb9f
Patrick-N-C <cowa0066@algonquinlive.com>
2019-03-28 12:08:03 -0400

    adding to map functionality for raids page

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { leaflet, icon, Marker } from 'leaflet';
+import '../../../node_modules/leaflet/dist/leaflet.js'
+//import { markers } from './markers.json';
 declare var L: any;
 @Component({
     selector: 'map',
@@ -15,6 +18,28 @@ export class MapComponent implements OnInit {
     public constructor() { }
     public ngOnInit() { }
     public ngAfterViewInit() {
+        L.Icon.Default.imagePath = ''
+
+        var markers = [
+            {
+              "name": "Raid 1",
+              "address": "",
+              "lat": 45.2775417,
+              "lng": -75.7393688
+            },
+            {
+              "name": "Raid 2",
+              "address": "",
+              "lat": 45.2717433,
+              "lng": -75.738532
+            },
+            {
+              "name": "Raid 3",
+              "address": "",
+              "lat": 45.2668505,
+              "lng": -75.7500547
+            }
+         ];
 
         var map = L.map( 'map', {
             center: [45.2748993, -75.743832],
@@ -25,5 +50,12 @@ export class MapComponent implements OnInit {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             subdomains: ['a','b','c']
         }).addTo( map );
+
+        for ( var i=0; i < markers.length; ++i ) 
+        {
+            L.marker( [markers[i].lat, markers[i].lng] )
+                .bindPopup( '<p>' + markers[i].name + '</p>' )
+                .addTo( map );
+        }
     }
 }
